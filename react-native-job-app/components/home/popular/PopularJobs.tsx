d235ed4b17329f9dc98158ca86d26703647b4072
kouglas <kouglas2@gmail.com>
2024-01-02 19:14:54 -0600

    edit popularJobs / welcome page

--- react-native-job-app/components/home/popular/PopularJobs.tsx
+++ react-native-job-app/components/home/popular/PopularJobs.tsx
@@ -8,12 +8,48 @@ import {
   FlatList,
   ActivityIndicator,
 } from "react-native";
+import styles from './popularJobs.style';
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 
 export default function PopularJobs() {
+  const Router = useRouter();
+
+  const isLoading = false
+
+  const error = false
+
   return (
-    <View>
-      <Text>Popular jobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size='large' colors={COLORS.primary} />  
+        ) : error ? (
+          <Text>theres a problem</Text>
+        ) : (
+          <FlatList
+            data={[1,2,3,4,5,6,7,8]}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+          />
+        )}
+              keyExtractor={item => item?.job_id}
+              contentContainerStyle={{ columnGap: SIZES.medium  }}
+              horizontal
+           />
+        )}
+
+      </View>
+      
     </View>
   )
 }
--- react-native-job-app/components/home/welcome/Welcome.tsx
+++ react-native-job-app/components/home/welcome/Welcome.tsx
@@ -13,8 +13,11 @@ import { useRouter } from 'expo-router'
 import styles from './welcome.style'
 import { icons, SIZES } from "../../../constants";
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
+
 export default function Welcome(){
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time")
 
   return (
     <View>
@@ -24,10 +27,41 @@ export default function Welcome(){
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput}/>
-
+          <TextInput 
+          style={styles.searchInput}
+          value=""
+          onChange={() =>{}}
+          placeholder="What job do you want?"
+          />
         </View>
 
+        <TouchableOpacity style={styles.searchBtn} onPress={() =>{}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={()=> {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text 
+                style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
       </View>
     </View>
   )
